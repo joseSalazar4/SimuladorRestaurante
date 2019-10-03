bc5713237a386defcffc969c86ff5aa7067e092d
Jose Salazar <jsalazarg48@gmail.com>
2019-10-03 04:15:21 -0600

    Added visuals

---
+++ comensalthread.cpp
@@ -0,0 +1,6 @@
+#include "comensalthread.h"
+
+ComensalThread::ComensalThread()
+{
+
+}
---
+++ comensalthread.h
@@ -0,0 +1,11 @@
+#ifndef COMENSALTHREAD_H
+#define COMENSALTHREAD_H
+
+
+class ComensalThread
+{
+public:
+    ComensalThread();
+};
+
+#endif // COMENSALTHREAD_H
